66b4ec91a475b739ff7d6c58dbb9cdcde44f6a06
delapublishing2 <164275271+delapublishing2@users.noreply.github.com>
2024-07-27 11:24:29 -0400

    commit

--- docs/js/server.js
+++ docs/js/server.js
@@ -3,6 +3,9 @@ const bodyParser = require('body-parser');
 const stripe = require('stripe')('process.env.STRIPE_SECRET_KEY');
 
 const app = express();
+
+
+
 app.use(bodyParser.json());
 
 app.post('/create-checkout-session', async (req, res) => {
