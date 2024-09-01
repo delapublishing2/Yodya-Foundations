826d5a70035d11fa6777055fa5518660f4787ac1
delapublishing2 <164275271+delapublishing2@users.noreply.github.com>
2024-09-01 17:53:30 -0400

    commit

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -153,3 +153,31 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
+
+    // Get the modal
+    var popup = document.getElementById("donationPopup");
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("openPopup");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on the button, open the modal
+    btn.onclick = function() {
+        popup.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        popup.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == popup) {
+            popup.style.display = "none";
+        }
+    }
+
+
