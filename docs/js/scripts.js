73628f2195bb6b21777c00093b89fa01e44d6405
delapublishing2 <164275271+delapublishing2@users.noreply.github.com>
2024-10-09 19:57:21 -0400

    commit

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -181,3 +181,18 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
 
+    document.getElementById("popupBtn").onclick = function() {
+    document.getElementById("popup").style.display = "block";
+};
+
+document.getElementById("closeBtn").onclick = function() {
+    document.getElementById("popup").style.display = "none";
+};
+
+window.onclick = function(event) {
+    if (event.target == document.getElementById("popup")) {
+        document.getElementById("popup").style.display = "none";
+    }
+};
+
+
