9daa7b79973a7ba80478349de42c7a75562555ff
delapublishing2 <164275271+delapublishing2@users.noreply.github.com>
2024-12-23 17:55:57 -0500

    commit

---
+++ docs/js/pop-up.js
@@ -0,0 +1,11 @@
+// Open the popup
+function openPopup() {
+    const popup = document.getElementById('imagePopup');
+    popup.style.display = 'flex'; // Show popup with flex layout
+}
+
+// Close the popup
+function closePopup() {
+    const popup = document.getElementById('imagePopup');
+    popup.style.display = 'none'; // Hide the popup
+}
